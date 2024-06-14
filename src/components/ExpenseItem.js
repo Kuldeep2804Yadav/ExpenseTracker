594d30561e77b9b8c1f5de88892c725e2a9618f8
Kuldeep2804Yadav <kuldeep2804yadav@gmail.com>
2024-06-15 00:26:39 +0530

    Added new Expanse

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -3,6 +3,12 @@ function ExpanseItem(){
         <p>Food - $5</p>
         <p>Petrol - $20</p>
         <p>Movie - $10</p>
+        <div>Date</div>
+        <div>
+            <h1>book</h1>
+            <div>$10</div>
+            <div>Delhi</div>
+        </div>
     </div>
 }
 
