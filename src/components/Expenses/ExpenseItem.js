51b20565ec5d5073edca274d530124068093cf5f
Kuldeep2804Yadav <kuldeep2804yadav@gmail.com>
2024-06-17 20:52:22 +0530

    added a ClickEvent

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,17 +1,20 @@
-import React from 'react';
+// Write your code at the relevant places in the code below
+
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 
 function ExpenseItem(props) {
-  
+  function buttonClickHandler(){
+    console.log("Button clicked")
+  }
   return (
     <Card className="expense-item">
-      <ExpenseDate date={props.date}/>
+      <ExpenseDate date={props.date} />
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.price}</div>
-        <div className="expense-item__location">{props.location}</div>
+        <button onClick={buttonClickHandler}>Change Title</button>
       </div>
     </Card>
   );
